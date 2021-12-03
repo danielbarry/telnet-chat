1d26ad1bcd737184efd42c1b720385393751ad60
Dan <danbarry16@googlemail.com>
2021-12-03 23:38:02 +1300

    Don't print extra new lines after messages as it breaks consistency

--- config.h
+++ config.h
@@ -7,13 +7,13 @@
 "| NOTE: All IPs displayed in chat!\n"\
 "| WARN: All data unencrypted!\n"     \
 "| Type `/h` for more info.\n"        \
-"| ------------------------------\n\n"\
+"| ------------------------------\n"  \
 )
 #define MSG_HELP (\
 "| Commands are as follows:\n"        \
 "| /b       - Display the banner\n"   \
 "| /h or /? - Display this help\n"    \
-"| /q       - Quit the connection\n\n"\
+"| /q       - Quit the connection\n"  \
 )
 #define MSG_EXIT (\
 "| Have a nice trip!\n"               \
