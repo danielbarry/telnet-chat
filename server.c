7afea90213e2bff96b78be85309e58ed805b14f9
Dan <danbarry16@googlemail.com>
2021-12-09 19:04:33 +1300

    Remove unused imports

--- server.c
+++ server.c
@@ -3,11 +3,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-//#include <netdb.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-//#include <sys/types.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
